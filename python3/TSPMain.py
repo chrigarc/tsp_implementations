8daeb7462f54d6521080965185fa2ed0574773fa
Christian Garcia <chrigarc@ciencias.unam.mx>
2020-10-06 18:03:35 -0500

    se agrego el codigo para que se puede ejecutar directamente en python2 y python3

--- python3/TSPMain.py
+++ python3/TSPMain.py
@@ -1,15 +1,15 @@
 from TSPInstance import TSPInstance
 import sys
 import os
+import numpy as np
 
 def main(argv):
     assert len(argv) != 0 , 'Se requiere el nombre del archivo'
     assert os.path.isfile(argv[0]), 'El archivo no existe'
     tspInstance = TSPInstance.readFile(argv[0])
     assert tspInstance, 'Error al leer la instancia'
-
-
-
+    print(tspInstance.nodes)
+    print(tspInstance.distancias)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
